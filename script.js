0c4ebe8b39af8d5b65382e4a57bf2ae6dbbda71e
eugeniakhaleeva <khaleeva20990@gmail.com>
2022-12-13 22:35:03 +0100

    fix: fixed switching active class for link in header menu

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.location.href='#about'
+    document.location.href = '#about'
 });
 
 const navLinks = document.querySelectorAll('.nav-link')
 
-for (let link of navLinks) {
-
-
-    link.addEventListener('click', function() {
-
-        if(link.classList.contains("active-link")){
-            link.classList.remove("active-link");
-        }
-
-        let setClasses = !this.classList.contains('active-link');
-        setClass(navLinks, 'active-link', 'remove');
-        if (setClasses) {
-            this.classList.add("active-link");
-        } else{
-            this.classList.remove("active-link");
-        }
-    })
+const result = [];
+for (const node of navLinks.values()) {
+    result.push(node)
 }
+
+function changeActiveClass(elem, className) {
+    if (elem.classList.contains(className)) {
+        elem.classList.remove(className);
+    }
+    let setClasses = !elem.classList.contains(className);
+    setClass(elem, className, 'remove');
+    if (setClasses) {
+        elem.classList.add(className);
+    } else {
+        elem.classList.remove(className);
+    }
+}
+
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY;
+
+    let sections = document.querySelectorAll('section')
+    sections.forEach(el => {
+            let top = el.offsetTop - 50;
+            let bottom = el.offsetHeight + top;
+            if (scrollTop > top && scrollTop < bottom) {
+                let id = el.getAttribute('id');
+                let currentLink = result.filter(i => i.hash.slice(1) === id)
+                currentLink.map(i => {
+                        if (i.classList.contains("active-link")) {
+                            i.classList.remove("active-link");
+                        }
+                        let setClasses = !i.classList.contains('active-link');
+                        setClass(navLinks, 'active-link', 'remove');
+                        if (setClasses) {
+                            i.classList.add("active-link");
+                        } else {
+                            i.classList.remove("active-link");
+                        }
+                    }
+                )
+
+            }
+        }
+    )
+});
+
+
+navLinks.forEach(link => link.addEventListener('click', function () {
+    changeActiveClass(link, 'active-link')
+}))
+
 
 function setClass(elem, className, action) {
     for (let i = 0; i < elem.length; i++) {
