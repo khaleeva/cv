edced0badb26ba731fa2db958e31ed129443cad9
eugeniakhaleeva <khaleeva20990@gmail.com>
2022-12-16 00:29:13 +0100

    fix: fixed script to change current link

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     document.location.href = '#about'
-// });
 
 const navLinks = document.querySelectorAll('.nav-link')
+const burgerNavLinks = document.querySelectorAll('.burger__menu-nav-link')
 const sections = document.querySelectorAll('section')
 const burger = document.querySelector('.burger__menu')
 const burgerMenu = document.querySelector('.burger__menu-nav')
-const header = document.querySelector('.header')
 const contentHeight = document.documentElement.clientHeight
-
 
 burger.addEventListener('click', function () {
     burger.classList.toggle('openMenu')
     burgerMenu.classList.toggle('active-menu')
     burgerMenu.style.height = contentHeight + 'px'
-
 })
 
 const result = [];
@@ -23,14 +18,17 @@ for (const node of navLinks.values()) {
     result.push(node)
 }
 
+for (const node of burgerNavLinks.values()) {
+    result.push(node)
+}
 
 
-function changeActiveClass(elem, className) {
+function changeActiveClass(classes, elem, className) {
     if (elem.classList.contains(className)) {
         elem.classList.remove(className);
     }
     let setClasses = !elem.classList.contains(className);
-    setClass(elem, className, 'remove');
+    setClass(classes, className, 'remove');
     if (setClasses) {
         elem.classList.add(className);
     } else {
@@ -38,45 +36,28 @@ function changeActiveClass(elem, className) {
     }
 }
 
-
-
 window.addEventListener('scroll', () => {
-   let scroll = window.pageYOffset
-
+    let scroll = window.pageYOffset
     sections.forEach(el => {
             let id = el.getAttribute('id');
             let top = el.offsetTop - 100;
             let bottom = el.offsetHeight + top;
-
-
-        let currentLink = result.filter(i => i.hash.slice(1) === id)
-
-
+            let currentLink = result.filter(i => i.hash.slice(1) === id)
             if (scroll > top && scroll < bottom) {
-                currentLink.map(i => {
-                        if (i.classList.contains("active-link")) {
-                            i.classList.remove("active-link");
-                        }
-                        let setClasses = !i.classList.contains('active-link');
-                        setClass(navLinks, 'active-link', 'remove');
-                        if (setClasses) {
-                            i.classList.add("active-link");
-                        } else {
-                            i.classList.remove("active-link");
-                        }
-                    }
-                )
-
+                currentLink.map(i => changeActiveClass(navLinks, i, 'active-link'))
+                currentLink.map(i => changeActiveClass(burgerNavLinks, i, 'active-link'))
             }
         }
     )
 });
 
-
 navLinks.forEach(link => link.addEventListener('click', function () {
-    changeActiveClass(link, 'active-link')
+    changeActiveClass(navLinks, link, 'active-link')
 }))
 
+burgerNavLinks.forEach(link => link.addEventListener('click', function () {
+    changeActiveClass(burgerNavLinks, link, 'active-link')
+}))
 
 function setClass(elem, className, action) {
     for (let i = 0; i < elem.length; i++) {
