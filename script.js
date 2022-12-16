4d1cb0940026a119c75da52323278815dc1723b9
eugeniakhaleeva <khaleeva20990@gmail.com>
2022-12-16 01:12:02 +0100

    feat: added hidden for arrow-btn, when page is scrolled

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
 const navLinks = document.querySelectorAll('.nav-link')
-const burgerNavLinks = document.querySelectorAll('.burger__menu-nav-link')
+const asideNavLinks = document.querySelectorAll('.arrow__menu-nav-link')
 const sections = document.querySelectorAll('section')
-const burger = document.querySelector('.burger__menu')
-const burgerMenu = document.querySelector('.burger__menu-nav')
+const arrow = document.querySelector('.arrow__menu')
+const asideMenu = document.querySelector('.arrow__menu-nav')
 const contentHeight = document.documentElement.clientHeight
 
-burger.addEventListener('click', function () {
-    burger.classList.toggle('openMenu')
-    burgerMenu.classList.toggle('active-menu')
-    burgerMenu.style.height = contentHeight + 'px'
+
+arrow.addEventListener('click', function () {
+    arrow.classList.toggle('openMenu')
+    asideMenu.classList.toggle('active-menu')
+    asideMenu.style.height = contentHeight + 'px'
 })
 
 const result = [];
@@ -18,7 +19,7 @@ for (const node of navLinks.values()) {
     result.push(node)
 }
 
-for (const node of burgerNavLinks.values()) {
+for (const node of asideNavLinks.values()) {
     result.push(node)
 }
 
@@ -36,8 +37,7 @@ function changeActiveClass(classes, elem, className) {
     }
 }
 
-window.addEventListener('scroll', () => {
-    let scroll = window.pageYOffset
+function checkPositionScrollToAddActiveClass (scroll) {
     sections.forEach(el => {
             let id = el.getAttribute('id');
             let top = el.offsetTop - 100;
@@ -45,18 +45,30 @@ window.addEventListener('scroll', () => {
             let currentLink = result.filter(i => i.hash.slice(1) === id)
             if (scroll > top && scroll < bottom) {
                 currentLink.map(i => changeActiveClass(navLinks, i, 'active-link'))
-                currentLink.map(i => changeActiveClass(burgerNavLinks, i, 'active-link'))
+                currentLink.map(i => changeActiveClass(asideNavLinks, i, 'active-link'))
             }
         }
     )
+}
+
+
+window.addEventListener('scroll', () => {
+    let scroll = window.pageYOffset
+    arrow.style.visibility = 'hidden'
+
+    setTimeout(() => {
+        arrow.style.visibility = 'visible'
+    }, 1000)
+
+    checkPositionScrollToAddActiveClass(scroll)
 });
 
 navLinks.forEach(link => link.addEventListener('click', function () {
     changeActiveClass(navLinks, link, 'active-link')
 }))
 
-burgerNavLinks.forEach(link => link.addEventListener('click', function () {
-    changeActiveClass(burgerNavLinks, link, 'active-link')
+asideNavLinks.forEach(link => link.addEventListener('click', function () {
+    changeActiveClass(asideNavLinks, link, 'active-link')
 }))
 
 function setClass(elem, className, action) {
